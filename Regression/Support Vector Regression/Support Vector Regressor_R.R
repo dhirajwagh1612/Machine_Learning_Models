819c9886f6bf74041c0c6ace2f560684f080473f
dhirajwagh1612 <54485930+dhirajwagh1612@users.noreply.github.com>
2021-04-19 22:11:28 +0530

    change

---
+++ Regression/Support Vector Regression/Support Vector Regressor_R.R
@@ -0,0 +1,31 @@
+#Support Vector Regressor.
+
+#reading dataset
+dataset = read.csv('Position_Salaries.csv')
+dataset = dataset[2:3]
+
+#fitting Support vector to the dataset.
+#install.packages('e1071')
+library(e1071)
+
+regressor = svm(formula = salary~.,
+                data = dataset,
+                type = 'eps-regression')
+
+
+# create regressor here.
+
+
+# predecting a new result.
+y_pred = predict(regressor, data.frame(level = 6.5))
+
+
+# visualise the SVR results.
+library(ggplot2)
+ggplot()+
+  geom_point(aes(x = dataset$Level, y= dataset$Salary), colour = 'red')+
+  geom_line(aes(x=dataset$Level, y= predict(regressor, newdata = dataset)), colour = 'blue')+
+  ggtitle('truth or bluff(SVR)')+
+  xlab('Level')+
+  ylab('salary')
+
