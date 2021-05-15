1caed7ea677b8203e99d293e819ad9a3d4bdb07f
dhirajwagh1612 <54485930+dhirajwagh1612@users.noreply.github.com>
2021-05-16 00:03:38 +0530

    Create HC.py

---
+++ Clustering/Hierarchical Clustering/HC.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 15 23:34:06 2021
+
+@author: Dhiraj Wagh
+"""
+# Importing Libraries.
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#Loading dataset
+dataset = pd.read_csv('Mall_Customers.csv')
+
+
+X=dataset.iloc[:, [3,4]].values
+
+# Using the dendrogram to find the optimal number of clusters.
+import scipy.cluster.hierarchy as sch
+dendrogram = sch.dendrogram(sch.linkage(X, method = 'ward'))
+plt.title('Dendrogram')
+plt.xlabel('Customers')
+plt.ylabel('Euclidean distances')
+plt.show()
+
+
+# fitting hierarchical clustering to the dataset.
+from sklearn.cluster import AgglomerativeClustering
+hc = AgglomerativeClustering(n_clusters=5, affinity='euclidean', linkage='ward')
+y_hc = hc.fit_predict(X)
+
+#visualization of clusters
+plt.scatter(X[y_hc == 0, 0], X[y_hc== 0,1],s= 100, c='red', label='Careful')
+plt.scatter(X[y_hc == 1, 0], X[y_hc== 1,1],s= 100, c='blue', label='standard')
+plt.scatter(X[y_hc == 2, 0], X[y_hc== 2,1],s= 100, c='green', label='Target')
+plt.scatter(X[y_hc == 3, 0], X[y_hc== 3,1],s= 100, c='cyan', label='Careless')
+plt.scatter(X[y_hc == 4, 0], X[y_hc== 4,1],s= 100, c='magenta', label='Sensible')
+
+
+plt.title('clusters of clients')
+plt.xlabel('Annual Income(k$')
+plt.ylabel('Spending Score (1-100')
+plt.legend()
+plt.show()
